76bf6a648e72288b5ed3b7bf63406d7db1c8c151
Kate Frolova <frolkate.alex@gmail.com>
2022-10-17 18:43:51 +0300

    feat: encrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,121 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  vigenereSquare = {
+    'A': [],
+    'B': [],
+    'C': [],
+    'D': [],
+    'E': [],
+    'F': [],
+    'G': [],
+    'H': [],
+    'I': [],
+    'J': [],
+    'K': [],
+    'L': [],
+    'M': [],
+    'N': [],
+    'O': [],
+    'P': [],
+    'Q': [],
+    'R': [],
+    'S': [],
+    'T': [],
+    'U': [],
+    'V': [],
+    'W': [],
+    'X': [],
+    'Y': [],
+    'Z': []
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     setVigenereSquare (){
+
+
+    let  vigenereSquareLetters = Object.keys(this.vigenereSquare);
+    let j = 0;
+
+    for(let i = 0; i < 26; i ++){
+       j = i;
+  while (this.vigenereSquare[vigenereSquareLetters[i]].length < 25){      this.vigenereSquare[vigenereSquareLetters[i]].push(vigenereSquareLetters[j]);
+    j++;
+    if(j == 26) j = 0;
+      }
+
+    }
+   }
+
+
+   isLetter(symbol){
+     return symbol.toUpperCase() != symbol.toLowerCase();
+   }
+
+
+
+  encrypt(message, key) {
+
+    this.setVigenereSquare();
+
+    //console.log(this.vigenereSquare['D']);
+  // console.log(this.vigenereSquare['A'].indexOf(''));
+    let lengthWord = message.length;
+    let word = '';
+    let i = 0;
+    let encryptWord = '';
+    let j = 0;
+
+    while(word.length < lengthWord){
+
+      if(this.isLetter(message[j])){
+        word += key[i];
+      }
+      else {
+        word += message[j];
+        i--;
+
+      }
+      j++;
+      if(i == key.length - 1 ) i = 0;
+      else i ++;
+
+
+      }
+    message = message.toUpperCase();
+    word = word.toUpperCase();
+
+
+        for(let i = 0; i < lengthWord; i ++){
+           if(this.isLetter(word[i])){
+
+          let ind = this.vigenereSquare['A'].indexOf(word[i]);
+          encryptWord+= this.vigenereSquare[message[i]][ind];
+           }
+          else
+             encryptWord+= word[i];
+        }
+
+    return encryptWord;
+  }
+  decrypt(encryptedMessage, key) {
+
   }
 }
 
